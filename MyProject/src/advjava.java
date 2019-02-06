5257473bc4109c3b2c940ebb3440f1b724bd92db
jayan <jayan@DESKTOP-4LEHD28>
2019-02-06 11:25:28 -0600

    first commit
    uploading all files

---
+++ MyProject/src/advjava.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class advjava {
+
+	public static void main(String[] args) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con;
+		
+	}
+
+}
